b1b3ae60a26f1991c8cdfcbf8ccbaf9b0ea972ca
Jasper van der Meer <jaspervdmeer@gmail.com>
2024-05-07 23:14:37 +0200

    Added new Component and more demos

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import CCategoryBlock from './components/CCategoryBlock.vue'
 import CCollage from './components/CCollage.vue'
 import CIcon from './components/CIcon.vue'
 import CPills from './components/CPills.vue'
+import CStreamerName from './components/CStreamerName.vue'
 import CText from './components/CText.vue'
 
 </script>
@@ -23,34 +24,130 @@ text-size: tiny, small, medium, large
 icon: any icones.js.org icon name, like mdi:twitch 
 text: any plain text you want to show on the bar</pre>
     </article>
-    <CBar 
-      type="plain" 
-      skin="white" 
-      size="medium" 
-      text-size="medium"
-      icon="ic:baseline-arrow-circle-right"
-      text="plain white medium medium" />
-    <CBar 
-      type="plain" 
-      skin="light" 
-      size="medium" 
-      text-size="medium"
-      icon="material-symbols-light:brightness-7"
-      text="plain light medium medium" />
-    <CBar 
-      type="plain" 
-      skin="dim" 
-      size="medium" 
-      text-size="medium"
-      icon="material-symbols:swipe-right"
-      text="plain dim medium medium" />
-    <CBar 
-      type="plain" 
-      skin="dark" 
-      size="medium" 
-      text-size="medium"
-      icon="material-symbols-light:brightness-4-outline"
-      text="plain dark medium medium" />
+    <div class="c-columns">
+      <div class="col">
+        <CBar 
+          type="plain" 
+          skin="white" 
+          size="tiny" 
+          text-size="tiny"
+          icon="ic:baseline-arrow-circle-right"
+          text="plain white tiny tiny" />
+        <CBar 
+          type="plain" 
+          skin="light" 
+          size="tiny" 
+          text-size="tiny"
+          icon="material-symbols-light:brightness-7"
+          text="plain light tiny tiny" />
+        <CBar 
+          type="plain" 
+          skin="dim" 
+          size="tiny" 
+          text-size="tiny"
+          icon="material-symbols:swipe-right"
+          text="plain dim tiny tiny" />
+        <CBar 
+          type="plain" 
+          skin="dark" 
+          size="tiny" 
+          text-size="tiny"
+          icon="material-symbols-light:brightness-4-outline"
+          text="plain dark tiny tiny" />
+      </div>
+      <div class="col">
+        <CBar 
+          type="plain" 
+          skin="white" 
+          size="small" 
+          text-size="small"
+          icon="ic:baseline-arrow-circle-right"
+          text="plain white small small" />
+        <CBar 
+          type="plain" 
+          skin="light" 
+          size="small" 
+          text-size="small"
+          icon="material-symbols-light:brightness-7"
+          text="plain light small small" />
+        <CBar 
+          type="plain" 
+          skin="dim" 
+          size="small" 
+          text-size="small"
+          icon="material-symbols:swipe-right"
+          text="plain dim small small" />
+        <CBar 
+          type="plain" 
+          skin="dark" 
+          size="small" 
+          text-size="small"
+          icon="material-symbols-light:brightness-4-outline"
+          text="plain dark small small" />
+      </div>
+    </div>
+    <div class="c-columns">
+      <div class="col">
+        <CBar 
+          type="plain" 
+          skin="white" 
+          size="medium" 
+          text-size="medium"
+          icon="ic:baseline-arrow-circle-right"
+          text="plain white medium medium" />
+        <CBar 
+          type="plain" 
+          skin="light" 
+          size="medium" 
+          text-size="medium"
+          icon="material-symbols-light:brightness-7"
+          text="plain light medium medium" />
+        <CBar 
+          type="plain" 
+          skin="dim" 
+          size="medium" 
+          text-size="medium"
+          icon="material-symbols:swipe-right"
+          text="plain dim medium medium" />
+        <CBar 
+          type="plain" 
+          skin="dark" 
+          size="medium" 
+          text-size="medium"
+          icon="material-symbols-light:brightness-4-outline"
+          text="plain dark medium medium" />
+      </div>
+      <div class="col">
+        <CBar 
+          type="plain" 
+          skin="white" 
+          size="large" 
+          text-size="large"
+          icon="ic:baseline-arrow-circle-right"
+          text="plain white large large" />
+        <CBar 
+          type="plain" 
+          skin="light" 
+          size="large" 
+          text-size="large"
+          icon="material-symbols-light:brightness-7"
+          text="plain light large large" />
+        <CBar 
+          type="plain" 
+          skin="dim" 
+          size="large" 
+          text-size="large"
+          icon="material-symbols:swipe-right"
+          text="plain dim large large" />
+        <CBar 
+          type="plain" 
+          skin="dark" 
+          size="large" 
+          text-size="large"
+          icon="material-symbols-light:brightness-4-outline"
+          text="plain dark large large" />
+      </div>
+    </div>
       <article>
       <h2>Cbar</h2>
       <h3>type: bar</h3>
@@ -97,22 +194,81 @@ text: any plain text</pre>
 text-size: tiny, small, medium, large 
 text: any plain text</pre>
     </article>
-    <CCategory 
-      skin="white"
-      text-size="medium"
-      text="World of Warcraft" />
-    <CCategory 
-      skin="light"
-      text-size="medium"
-      text="World of Warcraft" />
-    <CCategory 
-      skin="dim"
-      text-size="medium"
-      text="World of Warcraft" />
-    <CCategory 
-      skin="dark"
-      text-size="medium"
-      text="World of Warcraft" />
+    <div class="c-columns">
+      <div class="col">
+        <CCategory 
+          skin="white"
+          text-size="tiny"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="white"
+          text-size="small"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="white"
+          text-size="medium"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="white"
+          text-size="large"
+          text="World of Warcraft" />
+      </div>
+      <div class="col">
+        <CCategory 
+          skin="light"
+          text-size="tiny"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="light"
+          text-size="small"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="light"
+          text-size="medium"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="light"
+          text-size="large"
+          text="World of Warcraft" />
+      </div>
+      <div class="col">
+        <CCategory 
+          skin="dim"
+          text-size="tiny"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dim"
+          text-size="small"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dim"
+          text-size="medium"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dim"
+          text-size="large"
+          text="World of Warcraft" />
+      </div>
+      <div class="col">
+        <CCategory 
+          skin="dark"
+          text-size="tiny"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dark"
+          text-size="small"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dark"
+          text-size="medium"
+          text="World of Warcraft" />
+        <CCategory 
+          skin="dark"
+          text-size="large"
+          text="World of Warcraft" />
+      </div>
+    </div>
+    
       <article>
       <h2>CCategoryBlock</h2>
       <p>Options:</p>
@@ -350,6 +506,104 @@ text: any plain text</pre>
     text-size="large"
     skin="white"
     text="Can I finally beat this awesome game? Come hang out on stream!" />
+    <article>
+      <h2>CStreamerName</h2>
+      <p>Options:</p>
+<pre>text-size: tiny, small, medium, large 
+skin: white, light, dim, dark
+channel: streamer channel name
+icon: any icones.js.org icon name, like mdi:twitch</pre>
+    </article>
+    <div class="c-columns">
+      <div class="col">
+        <CStreamerName
+        text-size="tiny"
+        skin="white"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="small"
+        skin="white"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="medium"
+        skin="white"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="large"
+        skin="white"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+      </div>
+      <div class="col">
+        <CStreamerName
+        text-size="tiny"
+        skin="light"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="small"
+        skin="light"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="medium"
+        skin="light"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="large"
+        skin="light"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+      </div>
+      <div class="col">
+        <CStreamerName
+        text-size="tiny"
+        skin="dim"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="small"
+        skin="dim"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="medium"
+        skin="dim"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="large"
+        skin="dim"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+      </div>
+      <div class="col">
+        <CStreamerName
+        text-size="tiny"
+        skin="dark"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="small"
+        skin="dark"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="medium"
+        skin="dark"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+        <CStreamerName
+        text-size="large"
+        skin="dark"
+        channel="/Astronomoose"
+        icon="mdi:twitch" />
+      </div>
+    </div>
   </main>
 </template>
 
---
+++ src/components/CStreamerName.vue
@@ -0,0 +1,31 @@
+<script>
+import { Icon } from '@iconify/vue';
+export default {
+  props: {
+    textSize: String,
+    skin: String,
+    channel: String,
+    icon: String,
+  },
+  components: {
+    Icon
+  }
+}
+</script>
+
+<template>
+  <div class="c c-text" :data-text-size="textSize" :data-skin="skin">
+    <div class="c-columns">
+      <div class="col">
+        <span class="icon">
+          <Icon :icon="icon" style="width: 100%; height: 100%;" />
+        </span>
+      </div>
+      <div class="col">
+        <span class="text">
+          {{ channel }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
