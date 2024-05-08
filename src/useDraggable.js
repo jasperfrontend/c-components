a5b66e5eb419e28ca119c12d885f93bd09ffb33f
Jasper van der Meer <jaspervdmeer@gmail.com>
2024-05-08 23:45:29 +0200

    Touch Dragging

--- src/useDraggable.js
+++ src/useDraggable.js
@@ -35,6 +35,40 @@ export const useDraggable = ({gridSize}) => {
     window.addEventListener('mouseup', handleMouseUp)
   }
 
+  const handleTouchStart = (e) => { // afmaken
+    const touch = e.touches[0];
+
+    const startPos = {
+      x: touch.clientX - state.x,
+      y: touch.clientY - state.y,
+    };
+
+
+    const handleTouchMove = (e) => {
+      const touch = e.touches[0];
+
+      state.dragging = true
+      const dx = touch.clientX - startPos.x;
+      const dy = touch.clientY - startPos.y;
+      const snappedX = Math.round(dx / gridSize) * gridSize;
+      const snappedY = Math.round(dy / gridSize) * gridSize;
+      state.x = snappedX;
+      state.y = snappedY;
+      updateCursor();
+    };
+
+    const handleTouchEnd = () => {
+      document.removeEventListener('touchmove', handleTouchMove);
+      document.removeEventListener('touchend', handleTouchEnd);
+      resetCursor();
+
+      console.log('touchend event');
+    };
+
+    document.addEventListener('touchmove', handleTouchMove);
+    document.addEventListener('touchend', handleTouchEnd);
+  };
+
   const resetCursor = () => {
     document.body.style.cursor = 'default';
   }
@@ -44,9 +78,11 @@ export const useDraggable = ({gridSize}) => {
   }
   onMounted(() => {
     window.addEventListener('mousedown', handleMouseDown)
+    window.addEventListener('touchstart', handleTouchStart)
   })
   onUnmounted(() => {
     window.removeEventListener('mousedown', handleMouseDown)
+    window.removeEventListener('touchstart', handleTouchStart)
   })
   return {
       node,
